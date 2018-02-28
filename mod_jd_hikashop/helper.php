cd838ea9fc7a979b3744711f2b7ef78e1f735286
Sumesh577 <35709615+Sumesh577@users.noreply.github.com>
2018-03-01 05:28:55 +0530

    JD-Shop module For Hikashop
    JD-Shop module For Hikashop

---
+++ mod_jd_hikashop/helper.php
@@ -0,0 +1,42 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+ 
+class jdprice
+{
+	 public static  function getdata($selectcategory)
+		{	
+				$db = JFactory::getDbo();
+					$query = "select *
+						from #__hikashop_product_category pcat
+						JOIN
+						#__hikashop_product p
+						JOIN
+						#__hikashop_category c
+						JOIN
+						#__hikashop_file f
+						JOIN
+						#__hikashop_price 
+						JOIN 
+						#__hikashop_currency  
+						WHERE 
+						pcat.category_id = 	c.category_id
+						AND pcat.product_id = 	p.product_id 
+						AND f.file_ref_id = p.product_id
+						AND #__hikashop_price.price_product_id = p.product_id
+						AND #__hikashop_price.price_currency_id =#__hikashop_currency.currency_id
+						AND c.category_name='$selectcategory'
+						AND f.file_type='product'
+						AND p.product_published=1
+					    GROUP BY f.file_ref_id ORDER BY p.product_hit
+						LIMIT 0, 8";
+	
+				$db->setQuery($query);
+				$items = $db->loadAssocList();
+				return $items;		 
+		}
+ 
+}
+?>
---
+++ mod_jd_hikashop/tmpl/default.php
@@ -0,0 +1,132 @@
+<?php // no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+ 
+			$document = JFactory::getDocument(); 
+			$document->addStyleSheet('modules'.'/'.'mod_jd_hikashop'.'/'.'assets'.'/'.'css'.'/'.'slick.css');
+			$document->addStyleSheet('modules'.'/'.'mod_jd_hikashop'.'/'.'assets'.'/'.'css'.'/'.'slick-theme.css');
+			$document->addStyleSheet('modules'.'/'.'mod_jd_hikashop'.'/'.'assets'.'/'.'css'.'/'.'style.css');
+			$document->addScript('modules'.'/'.'mod_jd_hikashop'.DS.'assets'.DS.'js'.DS.'slick.min.js');
+			//$document->addScript('modules'.'/'.'mod_jd_hikashop'.DS.'assets'.DS.'js'.DS.'custom-script.js');
+ 
+?>
+     
+    <!---- Font Awesome ---->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <!-- Bootstrap -->
+ 
+<!---- <script type="text/javascript" src="modules/mod_jd_hikashop/assets/js/slick.min.js"></script> ---->
+  <div class="jd-hikashop-category-element">
+        
+            <div class="jd-category-slider jd-hikashop-category-wrapper">
+                <div class="row jd-slider-row">
+                    <div class="col-md-5 jd-slider-col">
+                        <div class="jd-product-heading-wrapper">
+                            <h1 class="jd-slider-heading"><?php echo $title;?></h1>
+                            <p class="jd-slider-subHeading"><?php echo $subtitle;?></p>
+                        </div>
+                    </div>
+                    <div class="col-md-7 jd-slider-col">
+                        <div class="jd-slider-category-item-list jd-hikashop-category-item-list">
+                            <ul class="jd-unstyled-list">
+								<?php if($featuredproducts=="1") :?>
+									<li class="active"><a href="#">Featured Product</a></li>
+								<?php endif;?>
+								
+								<?php if($newarrival=="1") :?>
+									<li><a href="#">New Arrival</a></li>
+								<?php endif;?>
+								
+								<?php if($hotproducts=="1") :?>
+									<li><a href="#">Hot Products</a></li>
+								<?php endif;?>
+								
+								<?php if($bestseller=="1") :?>
+									<li><a href="#">Best Seller</a></li>
+								<?php endif;?>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+				<?php if(empty($count)){ ?>
+					<div class="alert alert-error"><a data-dismiss="alert" class="close">x</a><div><p><?php echo "ADD PRODUCT"; ?></p></div></div>
+				<?php } ?>
+                <div class="jd-hikashop-category-container">
+                    <div class="jd-hikashop-category-item-wrapper">
+						<?php foreach($item as $items) :?>
+							<div class="jd-hikashop-category-item">
+								<div class="jd-hikashop-category-item-image text-center">
+									<a href="#"><img src="/joomla3/images/com_hikashop/upload/<?php echo $items['file_path']?>" alt=""></a>
+								</div>
+								<div class="jd-hikashop-category-item-content text-center">
+									<div class="jd-slider-cart-wrapper">
+										<ul class="jd-unstyled-list">
+											<li class="jd-icon-cart"><a href="<?php echo 'index.php/hikashop-menu-for-categories-listing/product/'.$items['product_id'].'-'. $items['product_alias']?>"></a></li>
+											<li class="jd-icon-heart"><a href="#"></a></li>
+										</ul>
+									</div>
+									<div class="jd-slider-rating">
+										<ul class="jd-unstyled-list">
+											<li><a href="#"><i class="fas fa-star"></i></a></li>
+											<li><a href="#"><i class="fas fa-star"></i></a></li>
+											<li><a href="#"><i class="fas fa-star"></i></a></li>
+											<li><a href="#"><i class="fas fa-star"></i></a></li>
+											<li><a href="#"><i class="far fa-star"></i></a></li>
+										</ul>
+									</div>
+									<h5 class="jd-hikashop-category-heading">
+										<a href="<?php echo 'index.php/hikashop-menu-for-categories-listing/product/'.$items['product_id'].'-'. $items['product_alias']?>"><?php echo $items['product_name'];?></a>
+									</h5>
+									<h5 class="jd-hikashop-category-heading">
+										<a href="#"><?php echo $items['category_name'];?></a>
+									</h5>
+									<p class="jd-hikashop-category-price">
+										<a href="<?php echo 'index.php/hikashop-menu-for-categories-listing/product/'.$items['product_id'].'-'. $items['product_alias']?>"><?php echo $items['currency_code'];?> <?php echo $items['currency_symbol'];?> <?php echo $items['product_sort_price']; ?>
+										</a>
+									</p>
+								</div>
+							</div>
+						<?php endforeach;?>
+                    </div>
+                </div>
+            </div>
+    </div>
+      <script>
+	    jQuery(document).ready(function($ ){
+    $('.jd-hikashop-category-item-wrapper').slick({
+        slidesToShow: <?php if($count > 1){ echo $count-1;} else {echo $count;}?>,
+        dots: <?php if($displaybullets=="1"){echo "true";} else { echo "false";}?>,
+        arrows: <?php if($displayarrows=="1"){echo "true";} else { echo "false";}?>,
+        touchMove: true,
+        autoplay: <?php if($autoplay=="1"){echo "true";} else { echo "false";}?>,
+        pauseOnHover: true,
+		speed: <?php if($autoplayspeedoptional){ echo $autoplayspeedoptional;} else{ echo "1000";} ?>,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3
+                }
+    },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });
+});
+      </script>
+	  
+ 
